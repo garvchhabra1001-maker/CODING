291a952e22f4c0b6013e50955a679f8ac5c9a0ea
garvchhabra1001-maker <garvchhabra5678@gmail.com>
2025-09-01 16:30:57 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -2,9 +2,10 @@
 
 int main() {
     printf("Even numbers between 100 and 200:\n");
-    for (int i = 100; i <= 200; i += 2) {
+    for (int i = 100; i <= 200; i ++) {
         printf("%d ", i);
     }
     printf("\n"); // Add a newline for better formatting
     return 0;
+
 }
